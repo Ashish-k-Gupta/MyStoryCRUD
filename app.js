07cbbda5e3af4873d3fae809353dee8509af3345
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2023-11-29 12:31:09 +0530

    added one file to gitignore

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs',exphbs({ defaultLayout: 'main', extname: '.hbs'}));
      app.set('view engine', '.hbs')
      
-// Routes
+// Routes  
 app.use('/', require('./routes/index') )
 
 const PORT  = process.env.PORT || 3000
